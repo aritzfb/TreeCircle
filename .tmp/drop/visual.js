2ba6b2502755371a32b6091e55ca164ae88ea91b
Aritz Francoy Barreiro <aritzfb@gmail.com>
2018-11-09 13:52:56 +0100

    Compiled

--- .tmp/drop/visual.js
+++ .tmp/drop/visual.js
@@ -2116,11 +2116,11 @@ var powerbi;
     (function (visuals) {
         var plugins;
         (function (plugins) {
-            plugins.testTooltip4696B540F3494FE5BA002362825DDE7D_DEBUG = {
-                name: 'testTooltip4696B540F3494FE5BA002362825DDE7D_DEBUG',
+            plugins.testTooltip4696B540F3494FE5BA002362825DDE7D = {
+                name: 'testTooltip4696B540F3494FE5BA002362825DDE7D',
                 displayName: 'Pie Charts Tree',
                 class: 'Visual',
-                version: '1.0.2',
+                version: '1.0.1',
                 apiVersion: '1.11.0',
                 create: function (options) { return new powerbi.extensibility.visual.testTooltip4696B540F3494FE5BA002362825DDE7D.Visual(options); },
                 custom: true
--- .tmp/precompile/visualPlugin.ts
+++ .tmp/precompile/visualPlugin.ts
@@ -1,9 +1,9 @@
 module powerbi.visuals.plugins {
-    export var testTooltip4696B540F3494FE5BA002362825DDE7D_DEBUG = {
-        name: 'testTooltip4696B540F3494FE5BA002362825DDE7D_DEBUG',
+    export var testTooltip4696B540F3494FE5BA002362825DDE7D = {
+        name: 'testTooltip4696B540F3494FE5BA002362825DDE7D',
         displayName: 'Pie Charts Tree',
         class: 'Visual',
-        version: '1.0.2',
+        version: '1.0.1',
         apiVersion: '1.11.0',
         create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.testTooltip4696B540F3494FE5BA002362825DDE7D.Visual(options),
         custom: true
